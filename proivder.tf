953773372b4d3acac5ca6e9159cfa034435e61af
Supraj Maripeddi <suprajmaripeddi@C02FN3Z4Q6LR.local>
2022-08-09 07:25:42 +0530

    modified the versoin

--- proivder.tf
+++ proivder.tf
@@ -1,9 +1,8 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      Version = "~>3.27"
-    }
+backend "s3" {
+    bucket = "github-actions-with-terraform"
+    key    = "s3/terraform.tfstate"
+    region = "us-east-1"
   }
   required_version = ">=0.14.9"
 }
