4a9b3689af6129bb6d6db58e6c305f80f727f347
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-09-05 12:28:59 +0530

    Update proivder.tf

--- proivder.tf
+++ proivder.tf
@@ -1,10 +1,3 @@
-terraform {
-# backend "s3" {
-#     bucket = "github-actions-with-terraform"
-#     key    = "s3/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
 provider "aws" {
   region  = "us-east-1"
 }
