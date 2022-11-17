ed2ac3fa9d4d37f391f79dfad8d19ef05925ff04
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-11-17 19:18:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,20 @@
 resource "aws_iam_role" "test_role" {
   name = "iam_depends"
+   assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 resource "aws_iam_policy" "policy" {
   name        = "iam-policy"
