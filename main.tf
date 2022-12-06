9bf74306273bab37a0982628bc601b6d7c612710
SuprajMaripeddi <Maripeddi.raju@gmail.com>
2022-12-06 13:27:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,11 @@ resource "aws_s3_bucket" "b" {
 provider "aws" {
   region =    "us-east-1"
 }
+terraform {
+  backend "s3" {
+    bucket = "supraj-terraform-staate"
+    key    = "aws.tfstate"
+    region = "us-east-1"
+  }
+}
+
